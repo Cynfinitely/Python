44e273622c499412a3d72545a71875acd2f7e536
Celal Yasin Nari <55275515+Cynfinitely@users.noreply.github.com>
2022-02-20 21:13:17 +0200

    Update Quiz.py

--- Beginner Projects/Quiz.py
+++ Beginner Projects/Quiz.py
@@ -4,17 +4,21 @@ print("Welcome the Quiz!")
 
 answer = input('Do you want to solve this quiz? Please write yes or no.')
 
+# This code is for answer 'no' . It will cause to end quiz.
 if answer.lower() != 'yes' :
     print('\nOkay , thats sad! Bye!')
     sys.exit()
 
 print('\nThats great! Lets start with the first question!')
+#score holder for calculate final point.
 score = 0
 
 answer = input("What does CPU stand for? ")
 
+#We need answer in lower case , thats why I used .lower()
 if answer.lower() == 'central processing unit':
     print('Correct!')
+    #correct answers will add 1 point to final result.
     score += 1
 else:
     print('Wrong!')
